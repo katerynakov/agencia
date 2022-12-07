56a9e01613e71bc277af967b9bcf4021d1abfd9e
katerynakov <108405187+katerynakov@users.noreply.github.com>
2022-12-07 23:12:06 +0200

    1

---
+++ ua/footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__body">
+                <div class="footer__logo"><a href="index.html"><img src="/img/logo.png" alt=""></a></div>
+                <div class="footer__contacts">
+                    <p>тел.: <a href="tel:+380933873555">(093) 387-35-55</a></p>
+                    <p>e-mail: <a href="mailto:odesarda@gmail.com">odesarda@gmail.com</a></p>
+                    <p>вул. Рішельєвська, 8, м. Одеса, Україна, 65026</p>
+                    <p class="footer__contacts_f-logo"><img  src="img/facebook.svg" alt=""></>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
